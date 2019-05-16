301e5b7755a4ef97add91f91163fde9ca56a0928
Miguel Alcaino <miguel.alcaino@ogb.cl>
2019-05-16 18:14:19 +0200

    Board has been implemented

---
+++ src/components/Elements/Board.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="row" v-for="i in rows" :key="i">
+            <EmptyBlock v-for="j in columns" :key="j" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import EmptyBlock from "./EmptyBlock";
+    export default {
+        name: "Board",
+        components: {EmptyBlock},
+        data: () => {
+            return {
+                rows: 20,
+                columns: 10
+            }
+        }
+    }
+</script>
+
+<style scoped>
+div.row{
+    margin-bottom: -4px;
+}
+</style>
---
+++ src/components/Elements/EmptyBlock.vue
@@ -0,0 +1,22 @@
+<template>
+    <img v-bind:src="image">
+</template>
+
+<script>
+    import imageBlock from '../../assets/empty-block.png'
+
+    export default {
+        name: "EmptyBlock",
+        data: () => {
+            return {
+                image: imageBlock
+            }
+        }
+    }
+</script>
+
+<style scoped>
+img{
+    width: 15px;
+}
+</style>
